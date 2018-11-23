de82a39439504eaa95b551c652658d35f1007838
woanjing <woanjing@gmail.com>
2018-11-22 18:20:27 -0600

    Initial commit

---
+++ code.js
@@ -0,0 +1,28 @@
+onEvent("BasketDaisy", "click", function() {
+  setScreen("BDaisy");
+  playSound("assets/category_digital/boing_1.mp3", false);
+});
+onEvent("ForrestGreen", "click", function(){
+setScreen("FGreen");
+playSound("assets/category_digital/boing_2.mp3", false);
+});
+onEvent("Gibera", "click", function() {
+  setScreen("GScreen");
+  playSound("assets/category_digital/bounce_1.mp3");
+});
+onEvent("Enchanting", "click", function(){
+setScreen("Enchant");
+playSound("assets/category_digital/bounce_2.mp3");
+});
+onEvent("YourAreLovely!", "click", function() {
+  setScreen("startScreen");
+});
+onEvent("StaysActive", "click", function() {
+  setScreen("startScreen");
+});
+onEvent("StaysHealthy", "click", function() {
+  setScreen("startScreen");
+});
+onEvent("YouRWonderful", "click", function() {
+  setScreen("startScreen");
+});
